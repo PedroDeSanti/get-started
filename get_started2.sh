28f30bc94c09b7516fd0318a50c0b388ae61fbdd
PedroDeSanti <phmartinsanti@gmail.com>
2024-09-16 01:13:59 -0300

    fix gum install

--- get_started2.sh
+++ get_started2.sh
@@ -109,6 +109,8 @@ gum_apt_install() {
 ###########################################
 
 if ! command -v gum &> /dev/null; then
+    apt_update
+    apt_install curl
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
     echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
