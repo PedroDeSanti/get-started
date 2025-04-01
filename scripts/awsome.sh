16b37cf74bf5f1aec59506c4bbb9317b8acb0f08
PedroDeSanti <phmartinsanti@gmail.com>
2025-04-01 22:59:56 +0200

    Add initial installation script for AwesomeWM and dependencies

---
+++ scripts/awsome.sh
@@ -0,0 +1,48 @@
+
+sudo apt install -y \
+        rofi \
+        acpi \
+        acpid \
+        upower \
+        lxappearance \
+        jq \
+        inotify-tools \
+        policykit-1-gnome \
+        xdotool \
+        xclip \
+        gpick \
+        ffmpeg \
+        blueman \
+        redshift \
+        pipewire \
+        pipewire-alsa \
+        pipewire-pulse \
+        alsa-utils \
+        brightnessctl \
+        feh \
+        maim \
+        mpv \
+        mpd \
+        mpc \
+        mpdris2 \
+        python3-mutagen \
+        ncmpcpp \
+        playerctl \
+
+
+# to compile awesomewm from source
+
+sudo apt update
+sudo apt install -y \
+        libxcb-cursor-dev \
+        libxcb-xtest0-dev \
+        libxcb-xinerama0-dev \
+        libxcb-keysyms1-dev \
+        libstartup-notification0-dev \
+        libxdg-basedir-dev \
+        libxcb-xrm-dev
+
+
+# for picom
+
+sudo apt install libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev
